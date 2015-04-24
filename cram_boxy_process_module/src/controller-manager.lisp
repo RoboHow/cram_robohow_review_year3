f66e1a7866f3d5f932f774cab7941426027b516b
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-04-24 13:38:07 +0200

    First brush over boxy-pm.

---
+++ cram_boxy_process_module/src/controller-manager.lisp
@@ -0,0 +1,69 @@
+;;; Copyright (c) 2015, Georg Bartels <georg.bartels@cs.uni-bremen.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name or Universitaet Bremen nor the names of its
+;;;       contributors may be used to endorse or promote products derived from
+;;;       this software without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+
+(in-package :boxy-pm)
+
+(defun switch-controllers (cm start stop)
+  (with-fields (ok)
+      (call-persistent-service 
+       cm
+       :start_controllers (map 'vector #'identity start)
+       :stop_controllers (map 'vector #'identity stop)
+       :strictness 1)
+    (unless ok
+      (error "Switching controllers failed. Start: ~a, stop: ~a." start stop))))
+
+(defgeneric get-pos-controller-names (name)
+  (:method ((name (eql :right)))
+    (list "r_arm_traj_controller"))
+  (:method ((name (eql :left)))
+    (list "l_arm_traj_controller"))
+  (:method ((name (eql :both)))
+    (concatenate 'list (get-pos-controller-names :left) (get-pos-controller-names :right))))
+
+(defgeneric get-vel-controller-names (name)
+  (:method ((name (eql :right)))
+    (list "r_arm_vel"))
+  (:method ((name (eql :left)))
+    (list "l_arm_vel"))
+  (:method ((name (eql :both)))
+    (concatenate 'list (get-vel-controller-names :left) (get-vel-controller-names :right))))
+
+(defun stop-controllers (cm &key (arms :both))
+  (let ((pos-ctrls (get-pos-controller-names arms))
+        (vel-ctrls (get-vel-controller-names arms)))
+    (switch-controllers cm nil (concatenate 'list pos-ctrls vel-ctrls))))
+
+(defun ensure-vel-controllers (cm &key (arms :both))
+  (let ((pos-ctrls (get-pos-controller-names arms))
+        (vel-ctrls (get-vel-controller-names arms)))
+    (switch-controllers cm vel-ctrls pos-ctrls)))
+
+(defun ensure-pos-controllers (cm &key (arms :both))
+  (let ((pos-ctrls (get-pos-controller-names arms))
+        (vel-ctrls (get-vel-controller-names arms)))
+    (switch-controllers cm pos-ctrls vel-ctrls)))
---
+++ cram_boxy_process_module/src/process-module.lisp
@@ -0,0 +1,74 @@
+;;; Copyright (c) 2015, Georg Bartels <georg.bartels@cs.uni-bremen.de>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name or Universitaet Bremen nor the names of its
+;;;       contributors may be used to endorse or promote products derived from
+;;;       this software without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+
+(in-package :boxy-pm)
+
+(defstruct (boxy-pm-handle (:conc-name boxy-))
+  (controller-manager nil))
+
+(defun init-boxy-pm-handle ()
+  (let ((controller-manager
+          (make-instance
+           'persistent-service
+           :service-name "/controller_manager/switch_controller"
+           :service-type "controller_manager_msgs/SwitchController")
+          ))
+    (make-boxy-pm-handle :controller-manager controller-manager)))
+
+(defun cleanup-boxy-pm-handle (handle)
+  (close-persistent-service (boxy-controller-manager handle)))
+(defun enable-
+(defgeneric call-action (action &rest params))
+
+(defmacro def-action-handler (name args &body body)
+  (alexandria:with-gensyms (action-sym params)
+    `(defmethod call-action ((,action-sym (eql ',name)) &rest ,params)
+       (destructuring-bind ,args ,params ,@body))))
+
+(def-action-handler home (arm)
+  (ros-info (perception) "Homing ~a arm" arm)
+  ;;TODO(Georg): implement me
+)
+
+(def-process-module boxy-process-module (desig)
+  (apply #'call-action (reference desig)))
+
+(def-fact-group boxy-pm-action-designators (action-desig)
+
+  (<- (action-desig ?designator (home ?side))
+    (action-desig? ?designator)
+    (desig-prop ?designator (:to :home))
+    (desig-prop ?designator (:arm ?side))))
+
+
+(def-fact-group boxy-process-module (matching-process-module available-process-module)
+
+  (<- (matching-process-module ?designator robosherlock-mini-process-module)
+    (or (desig-prop ?designator (:to :home))))
+
+  (<- (available-process-module boxy-process-module)
+    (crs:true)))
